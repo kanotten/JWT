728c30d462d0c34cc432c35007c0699408fb1f0b
Sheikh <kenneth.sheikh@hotmail.com>
2025-01-17 11:54:28 +0100

    added checks for if user is correct user

--- index.js
+++ index.js
@@ -6,8 +6,34 @@ const PORT = 3000;
 
 const app = express();
 
+const users = [
+  {
+    username: "kenshe",
+    password: "holyludvig",
+  },
+  {
+    username: "baloo",
+    password: "maribo",
+  },
+];
+
+// Middleware to parse JSON body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello maan again");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  for (const user of users) {
+    if (user.username === username && user.password === password) {
+      return true;
+    }
+  }
+
+  return false;
 });
 
 app.listen(PORT, () => {
